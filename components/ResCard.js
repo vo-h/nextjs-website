e974421d200bd5b0c276d7d7771ca2b9b6a9be65
Hien Vo <hvo@uchicago.edu>
2022-01-04 22:43:39 -0800

    automated the tweaking of skills & projects

--- components/ResCard.js
+++ components/ResCard.js
@@ -6,10 +6,10 @@ export default function ResCard(props) {
     <Row className="">
       <Col className="">
       <Card style={{backgroundColor: "#292421"}}>
-        <Card.Img src={props.img}/>
+        <Card.Img src={props.img} style={{maxHeight: 250, width:"auto"}}/>
         <Card.Body style={{backgroundColor: "#1C1F23", color: "white"}}>
           <Card.Title className="pb-2">{props.title}</Card.Title>
-          <Card.Text>{props.text}</Card.Text>
+          <Card.Text style={{minHeight:150}}>{props.text}</Card.Text>
         <Card.Footer className="d-flex justify-content-end">
           <a href={props.link} target="_blank" rel="noreferrer">
             <Image src="link_white.webp" alt="Link icon" style={{maxWidth:20, heigh:"auto"}} className="mx-2" flex/>
--- pages/projects.js
+++ pages/projects.js
@@ -2,8 +2,10 @@ import {Card, Container, Row, Col} from 'react-bootstrap';
 import Expo from '../components/Expo'
 import ResCard from '../components/ResCard'
 import NavBar from '../components/NavBar'
-
-export default function Projects() {
+import {Client} from "@notionhq/client"
+import {parseProjects} from '../lib/others'
+import {getRecords} from '../lib/notion-comm'
+export default function Projects(props) {
 
   const short_intro=`Below are the things that I've done that are worth showing according to my arbitrary standards :)`
 
@@ -13,30 +15,36 @@ export default function Projects() {
       <Expo headline="Projects" text={short_intro}/>
       <hr/>
       <Container>
-        <Row>
-          <Col lg>
-
-            <ResCard
-              img="https://repository-images.githubusercontent.com/395060155/31d699b4-7f31-4477-8d30-ebc5ce164485"
-              title="Chicago Crime"
-              text="A dashboard of crime in Chicago & Hyde Park that queries data from Google Bigquery using pandas, plotly, and streamlit. Deployed on Heroku."
-              link="https://chicago-crime-dashboard.herokuapp.com/"
-              github="https://github.com/vo-h/Chicago-Crime"
-            />
-            </Col>
-          <Col lg>
-          <ResCard
-            img="https://repository-images.githubusercontent.com/443938170/a5a6a4b2-f3fa-4c0c-be3a-70c8c4c58afc"
-            title="NextJs Website"
-            text="A blog & resume website using NextJs, React-Bootstrap and a smattering of ther tools. Deployed on Vercel."
-            link="https://nextjs-website-vo-h.vercel.app/"
-            github="https://github.com/vo-h/nextjs-website"
-          />
-          </Col>
-
-        </Row>
+        {
+          props.projects.map((projectGroups, index) =>
+            <Row key={index}>
+              {
+                projectGroups.map(project =>
+                  <Col lg={6} key={project.title}>
+                    <ResCard
+                      img={project.img}
+                      title={project.title}
+                      text={project.text}
+                      link={project.link}
+                      github={project.github}
+                    />
+                  </Col>
+                )
+              }
+            </Row>
+          )
+        }
       </Container>
 
     </Container>
   )
+}
+
+export async function getStaticProps() {
+
+  const notion = new Client({auth: process.env.NOTION_KEY})
+  const records = await getRecords(process.env.NOTION_PROJECTS_DB, notion)
+  const projects = await parseProjects(records)
+
+  return {props: {projects: projects}}
 }
